6a93072e441d43c2202e3131aa993706e5d79779
Denis Mariga <denismariga50@gmail.com>
2023-02-20 12:09:01 +0300

    added a controller

--- src/main/java/com/denis/todoapp/Controller/firstpage.java
+++ src/main/java/com/denis/todoapp/Controller/firstpage.java
@@ -1,4 +1,12 @@
 package com.denis.todoapp.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class firstpage {
+    @RequestMapping("/firstpage")
+    public String app(){
+        return "index";
+    }
 }

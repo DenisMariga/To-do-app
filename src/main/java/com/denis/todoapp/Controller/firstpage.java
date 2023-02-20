10a0f4983c87d4dce5eea03446a5b9ca78373b1b
Denis Mariga <denismariga50@gmail.com>
2023-02-20 12:47:34 +0300

    added a logs

--- src/main/java/com/denis/todoapp/Controller/firstpage.java
+++ src/main/java/com/denis/todoapp/Controller/firstpage.java
@@ -1,12 +1,17 @@
 package com.denis.todoapp.Controller;
 
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 public class firstpage {
+    private static final Logger log= (Logger) LoggerFactory.getLogger(firstpage.class);
     @RequestMapping("/firstpage")
     public String app(){
+        log.info("name");
         return "index";
     }
 }
